ea4d33f85fd9e6c9b0c0293bcfc74e73f68323a1
Alex Butirskiy <butirskiy@gmail.com>
2015-08-31 22:37:19 +0300

    SQL task updated

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,19 +1,22 @@
 user = User.new
-user.email = 'user@exemple.com'
+user.email = 'user@example.com'
 user.password = 'password'
 user.password_confirmation = 'password'
 user.save!
 
 Project.create!([
   {name: "Complete the test task for RubyGarage", user_id: 1},
-  {name: "New Project", user_id: 2},
-  {name: "New Project", user_id: 2},
-  {name: "New Project", user_id: 2},
-  {name: "New Project", user_id: 2}
+  {name: "For Home", user_id: 1},
 ])
 Task.create!([
   {name: "Open mockup", status: nil, project_id: 1},
   {name: "Attentively check the file", status: nil, project_id: 1},
   {name: "Write HTML & CSS", status: nil, project_id: 1},
   {name: "Add Javascript", status: nil, project_id: 1}
+])
+Task.create!([
+  {name: "Buy milk", status: nil, project_id: 2},
+  {name: "Call Mam", status: nil, project_id: 2},
+  {name: "Clean the room", status: nil, project_id: 2},
+  {name: "Repair the DVD player", status: nil, project_id: 2}
 ])
