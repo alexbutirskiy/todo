07a6be7ea40678d28f2b09b1ed1ab888f87e0b9d
Alex Butirskiy <butirskiy@gmail.com>
2015-09-29 23:19:22 +0300

    Some styles added

--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -4,4 +4,23 @@ module TasksHelper
     return datetime.to_date.to_s if datetime.hour == 0 && datetime.min == 0
     datetime.to_s
   end
+
+  def attachment_class(comment)
+    case comment.attachment_content_type
+    when "text/plain"
+      "text_plain"
+    when "application/pdf"
+      "application_pdf"
+    when "application/pdf"
+      "application_pdf"
+    when "application/zip"
+      "application_zip"
+    when "application/msword",
+         "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+      "application_msword"
+    when "application/vnd.ms-excel"
+      "application_vnd_ms_excel"
+    else "class_unknown"
+    end
+  end
 end
