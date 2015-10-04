ea451aaa8e8f428b144218fbca0f8caf8baf96e3
Alex Butirskiy <butirskiy@gmail.com>
2015-10-04 21:51:42 +0300

    Google and GitHub authorization added

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,30 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  devise :omniauthable, omniauth_providers: [:facebook]
+  devise :omniauthable, omniauth_providers: [:facebook, :google_oauth2, :github]
   
   has_many :projects
 
   def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.password = Devise.friendly_token[0, 20]
+
+    user = where(provider: auth.provider, uid: auth.uid).first
+
+    unless user
+      user = find_by(email: auth.info.email)
+      unless user
+        #byebug
+        user = create(provider: auth.provider, uid: auth.uid, 
+                      email: auth.info.email, password: Devise.friendly_token[0, 20])
+      end
     end
+
+    user
+
+    # where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    #   byebug
+    #   user.email = auth.info.email
+    #   user.password = Devise.friendly_token[0, 20]
+    # end
   end
 
 end
